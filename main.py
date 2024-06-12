b642bd1a06227781f7a1d9724cffced78bae49ce
Lawrence M <lawrencejunzh@gmail.com>
2024-06-12 16:25:53 -0400

    bubble sort

--- main.py
+++ main.py
@@ -7,17 +7,17 @@ print("hello world")
 def bubble_sort(arr):
     n = len(arr)
     # Traverse through all array elements
-    for i in range(n):
+    for o in range(n):
         # Last i elements are already in place
-        for j in range(0, n - i - 1):
-            # Traverse the array from 0 to n-i-1
+        for j in range(0, n - o - 1):
             # Swap if the element found is greater than the next element
             if arr[j] > arr[j + 1]:
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
+                print(5)
     return arr
 
 
 # Example usage
 my_list = [64, 34, 25, 12, 22, 11, 90]
 sorted_list = bubble_sort(my_list)
-print("Sorted list is:", sorted_list)
+print("bruh")
